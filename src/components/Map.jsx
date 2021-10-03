d1c90c1834ba7846779789f6c7fd787072bac233
Adam Cottrill <racottrill@gmail.com>
2021-10-03 08:21:18 -0400

    Memomized Make Marker function for improved performance.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -21,59 +21,63 @@ export const Map = () => {
     getSamples(mu_type)
   );
 
-  let markers = [];
-  // Only rerender markers if props.data has changed
-  if (data) {
-    const distinct = {};
-    data.forEach((sam) => {
-      const pt = `${sam.dd_lat.toFixed(3)}-${sam.dd_lon.toFixed(3)}`;
-      distinct[pt] = { dd_lat: sam.dd_lat, dd_lon: sam.dd_lon, mu: sam.area };
+  const useDistinctQuery = () =>
+    useQuery(["samples", mu_type], () => getSamples(mu_type), {
+      select: React.useCallback((data) => {
+        const distinct = {};
+        data.forEach((sam) => {
+          const pt = `${sam.dd_lat.toFixed(3)}-${sam.dd_lon.toFixed(3)}`;
+          distinct[pt] = {
+            dd_lat: sam.dd_lat,
+            dd_lon: sam.dd_lon,
+            mu: sam.area,
+          };
+        });
+        return distinct;
+      }, []),
     });
 
-    const mus = [...new Set(data.map((x) => x.area))];
+  const pts = useDistinctQuery();
 
-    // create an object that will map unique mu's to fill color
+  const makeMarkers = (pts) => {
+    let myMarkers = [];
+    let mus2 = [];
     const fillMap = {};
-    mus.forEach((x, i) => {
-      fillMap[x] = x ? colours[i % colours.length] : "#ffffff";
-    });
 
-    // for (const [key, val] of Object.entries(distinct)) {
-    //   markers.push(
-    //     <Marker key={key} longitude={val.dd_lon} latitude={val.dd_lat}>
-    //       <div>
-    //         <svg>
-    //           <circle
-    //             cx="5"
-    //             cy="5"
-    //             r="5"
-    //             fill={fillMap[val.mu]}
-    //             stroke="black"
-    //             opacity="0.3"
-    //           />
-    //         </svg>
-    //       </div>
-    //     </Marker>
-    //   );
-    // }
+    if (pts.data) {
+      let tmp = [];
+      for (const [key, val] of Object.entries(pts.data)) {
+        tmp.push(val.mu);
+      }
+      mus2 = [...new Set(tmp)];
 
-    markers = data.map((x) => (
-      <Marker key={x.slug} longitude={x.dd_lon} latitude={x.dd_lat}>
-        <div>
-          <svg>
-            <circle
-              cx="5"
-              cy="5"
-              r="5"
-              fill={fillMap[x.area]}
-              stroke="black"
-              opacity="0.3"
-            />
-          </svg>
-        </div>
-      </Marker>
-    ));
-  }
+      mus2.forEach((x, i) => {
+        fillMap[x] = x ? colours[i % colours.length] : "#ffffff";
+      });
+
+      for (const [key, val] of Object.entries(pts.data)) {
+        myMarkers.push(
+          <Marker key={key} longitude={val.dd_lon} latitude={val.dd_lat}>
+            <div>
+              <svg>
+                <circle
+                  cx="5"
+                  cy="5"
+                  r="5"
+                  fill={fillMap[val.mu]}
+                  stroke="black"
+                  opacity="0.3"
+                />
+              </svg>
+            </div>
+          </Marker>
+        );
+      }
+    }
+    return myMarkers;
+  };
+
+  const markers2 = React.useMemo(() => makeMarkers(pts), [pts]);
 
   const defaults = new WebMercatorViewport({
     width: 1000,
@@ -96,7 +100,7 @@ export const Map = () => {
       mapboxApiAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
     >
-      {data && markers}
+      {data && markers2}
     </ReactMapGL>
   );
 };
